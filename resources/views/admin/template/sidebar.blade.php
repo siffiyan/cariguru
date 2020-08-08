c910da210284422c1c1e0dcdfe3cb7761aff6cc5
armadhilo <dunsforce@gmail.com>
2020-08-08 14:40:43 +0700

    fix template src

--- resources/views/admin/template/sidebar.blade.php
+++ resources/views/admin/template/sidebar.blade.php
@@ -12,6 +12,15 @@
 <li> 
 <a href="/admin/manajemen_transaksi"><span>Manajemen Transaksi</span></a>
 </li>
+    <li> 
+        <a href="/admin/manajemen_transaksi"><span>Belajar dan Pembelajaran</span></a>
+    </li>
+    <li class="submenu">
+        <a href="#"><span> Reports</span> <span class="menu-arrow"></span></a>
+        <ul style="display: none;">
+            <li><a href="invoice-report.html">Invoice Reports</a></li>
+        </ul>
+    </li>
 </ul>
 </div>
 </div>
